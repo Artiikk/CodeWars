690b5646e17cd24ead15248924436d624ae6b130
Artur Shestopalov <artiikk2@gmail.com>
2021-08-28 18:00:23 +0300

    added: isPrime func

---
+++ isPrime.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num - positive integer
+ */
+ function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false
+  }
+  return num > 1
+}
+
+console.log(isPrime(7))
