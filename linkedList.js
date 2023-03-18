1caa68d2003ff028592c4a3123b5b1128ed62efb
Artur Shestopalov <artiikk2@gmail.com>
2023-03-18 13:25:07 +0100

    added: linked list implementation using vanilla js

---
+++ linkedList.js
@@ -0,0 +1,47 @@
+class LinkedList {
+  constructor(head = null) {
+      this.head = head;
+  }
+
+  size() {
+    let count = 0; 
+    let node = this.head;
+    while (node) {
+        count++;
+        node = node.next
+    }
+    return count;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  getLast() {
+    let lastNode = this.head;
+    if (lastNode) {
+        while (lastNode.next) {
+            lastNode = lastNode.next
+        }
+    }
+
+    return lastNode
+  }
+
+  getFirst() {
+    return this.head;
+  }
+}
+
+class ListNode {
+  constructor(val) {
+      this.val = val;
+      this.next = null;               
+  }
+}
+
+const node1 = new ListNode(2);
+const node2 = new ListNode(5);
+node1.next = node2;
+
+const list = new LinkedList(node1);
