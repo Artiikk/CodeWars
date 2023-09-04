7f9bd116626ccf613e937b3abf58576831d1e6ec
Artur Shestopalov <artiikk2@gmail.com>
2023-09-04 18:56:57 +0200

    added: composition function

---
+++ functionComposition.js
@@ -0,0 +1,14 @@
+const compose = function(functions) {
+	return function(x) {
+        return functions.reduceRight((acc, currFunction) => {
+            acc = currFunction(acc);
+
+            return acc;
+        }, x);
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
