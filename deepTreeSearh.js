105b47777e35f1d5d3d458fce61d9159ce1019ba
Artur Shestopalov <artiikk2@gmail.com>
2023-10-21 19:54:18 +0200

    added 'deepTreeSearch' function

---
+++ deepTreeSearh.js
@@ -0,0 +1,43 @@
+const tree = {
+  value: 1,
+  children: [
+    {
+      value: 2,
+      children: [{ value: 3 }],
+    },
+    {
+      value: 4,
+      children: [{ value: 5 }, { value: 6 }],
+    },
+  ],
+};
+
+function getTreeValues(tree) {
+  const stack = [tree];
+  const result = [];
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+
+    if (node.value) result.push(node.value);
+    if (node.children?.length) stack.push(...node.children);
+  }
+
+  return result;
+}
+
+console.log(getTreeValues(tree)); // [1, 2, 3, 4, 5, 6]
+
+function getTreeValuesRecursive(node, result = []) {
+  if (node.value) result.push(node.value);
+  
+  if (node.children?.length) {
+    for (const child of node.children) {
+      getTreeValuesRecursive(child, result);
+    }
+  }
+
+  return result;
+}
+
+console.log(getTreeValuesRecursive(tree)); // [1, 2, 3, 4, 5, 6]
